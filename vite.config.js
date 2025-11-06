14ae2676f3ff9203176fdecf7c01bedabc0093e4
andresaoa <andresrendon@aoacolombia.com>
2025-11-05 23:03:18 -0500

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,19 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: "/", // 👈 Esto es lo que Render necesita
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: false,
       proxy: {
         "/api": {
           target: "http://localhost:5000",
           changeOrigin: true,
-          secure: true,
+          secure: false,
         },
       },
     },
